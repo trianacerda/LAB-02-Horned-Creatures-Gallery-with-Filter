a6a46b763f989dbe80a2c9f84306c3a099c87b4d
Triana Cerda <trianaRcerda@gmail.com>
2021-08-03 17:09:01 -0700

    working .filter with multiple filter searches

--- src/App.js
+++ src/App.js
@@ -17,15 +17,13 @@ class App extends Component {
     this.setState({ horns: event.target.value });
   }
    render() { 
+    const keywords = ['All Creatures', 'narwhal', 'rhino', 'unicorn', 'unilego', 'triceratops', 'markhor', 'mouflon', 'addax', 'chameleon', 'lizard', 'dragon'];
+    const hornsOptions = ['All Creatures',1,2,3,100]
     const filteredCreatures = images.filter(
-      (image)=> image.keyword === this.state.keyword || this.state.keyword === 'All Creatures' 
+      (image)=> (image.keyword === this.state.keyword || this.state.keyword === 'All Creatures' ) && ( Number(image.horns) === Number(this.state.horns) || this.state.horns === 'All Creatures' )
     );
-    const filteredHorns = images.filter(
-      (horns)=> horns.keyword === this.state.horns || this.state.horns === 'All Creatures' 
-    );
-    console.log(filteredHorns)
-    const keywords = ['pick one', 'narwhal', 'rhino', 'unicorn', 'unilego', 'triceratops', 'markhor', 'mouflon', 'addax', 'chameleon', 'lizard', 'dragon'];
-    const horns = ['pick one',1,2,3,100]
+   
+    console.log(filteredCreatures)
     return ( 
       <div className="App">
         <h1>--HORNED CREATURES--</h1>
@@ -38,7 +36,7 @@ class App extends Component {
           </select>
           <p>HORNS</p>
           <select onChange={this.pickHorns}>
-          {horns.map((item, index) => (
+          {hornsOptions.map((item, index) => (
             <option key={index} value={item}>{item}</option>
           ))
           }
@@ -46,7 +44,6 @@ class App extends Component {
           
         <ImageList
         creaturesOnPage={filteredCreatures}
-        hornsOnPage={filteredHorns}
         />
       </div>
      );
